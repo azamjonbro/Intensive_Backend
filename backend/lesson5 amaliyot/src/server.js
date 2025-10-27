55337f41d5ac530b546821c9589d75efdc5cef75
azamjonbro <azamjondasturchi11@gmail.com>
2025-10-27 12:26:10 +0500

    added

--- backend/lesson5 amaliyot/src/server.js
+++ backend/lesson5 amaliyot/src/server.js
@@ -4,15 +4,15 @@ const app = express();
 
 const connectDB = require('./config/dbconnection');
 const userRouter = require('./router/user.router');
-
+const cors = require("cors")
 require('dotenv').config();
 
 // Connect to MongoDB
 connectDB();
 
+app.use(cors())
 // Middleware to parse JSON requests
 app.use(express.json());
-
 // User routes
 app.use('/api', userRouter);
 
