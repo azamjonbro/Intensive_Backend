238591be4b923603aa4a7b3bc5e70f377931c432
azamjonbro <azamjondasturchi11@gmail.com>
2025-11-26 11:40:19 +0500

    added

--- backend/lesson8/src/config/createSuperAdmin.js
+++ backend/lesson8/src/config/createSuperAdmin.js
@@ -1,5 +1,6 @@
 const superAdminModel = require("../models/superAdmin")
 const bcrypt = require("bcrypt")
+const jwt  = require("jsonwebtoken")
 
 const createSuperAdmin = async ()=>{
     try {
@@ -25,5 +26,46 @@ const createSuperAdmin = async ()=>{
         
     }
 }
+const loginSuperAdmin = async (req, res) => {
+    try {
+        const { username, password } = req.body;
 
-module.exports={createSuperAdmin}
+        // 1) Foydalanuvchi qidirish
+        const user = await superAdminModel.findOne({ username });
+        
+        if (!user) {
+            return res.status(404).json({
+                message: "superAdmin topilmadi"
+            });
+        }
+
+        // 2) Parolni solishtirish
+        const validPassword = await bcrypt.compare(password, user.password);
+
+        if (!validPassword) {
+            return res.status(401).json({
+                message: "Unauthorized"
+            });
+        }
+
+        // 3) Token yaratish
+        const token = jwt.sign(
+            { id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: "15d" }
+        );
+
+        return res.status(200).json({
+            message: "Successfully login",
+            token
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
+
+module.exports={createSuperAdmin, loginSuperAdmin}
--- backend/lesson8/src/index.js
+++ backend/lesson8/src/index.js
@@ -2,14 +2,14 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const {createSuperAdmin} = require("./config/createSuperAdmin")
+const {createSuperAdmin,loginSuperAdmin} = require("./config/createSuperAdmin")
 const {connectingDB,disConnectingDB} = require("./config/connectionDB")
 const adminRouter = require("./router/admin.router")
 
 app.use(cors());
 app.use(express.json());
 app.use("/admin", adminRouter)
-
+app.post("/kirish",loginSuperAdmin)
 
 app.listen(process.env.PORT, ()=>{
     connectingDB()
